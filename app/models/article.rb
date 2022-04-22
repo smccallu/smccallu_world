cb3dc5e55c9b318db30938e2611367ed0a81342e
Edward Chen <edwardch@andrew.cmu.edu>
2022-04-21 20:12:18 -0400

    article created

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    belongs_to :category
+
+    validates_presence_of :title, :content
+    scope :alphabetical,  -> { order(:title) }
+    scope :active,      -> { where(active: true) }
+end
